126d4997ce2bf4856b593f3caef83e22bb956118
damaya <damaya@aplyca.com>
2015-02-09 20:02:50 -0500

    ajustes pruebas y metodos

--- user/config.php
+++ user/config.php
@@ -6,10 +6,19 @@
  * Time: 12:23 AM
  */
 
-$ymlDatabase ="/vagrant/config/database.yml";
-$dbParams = yaml_parse_file($ymlDatabase);
-$serverName = $dbParams[db][dev][host];
-$user = $dbParams[db][dev]['user'];
-$password = $dbParams[db][dev]['password'];
-$database = $dbParams[db][dev]['database'];
-$usersTable = $dbParams[db][users]['table'];
+    /*
+    $ymlDatabase ="/vagrant/config/database.yml";
+    $dbParams = yaml_parse_file($ymlDatabase);
+    $serverName = $dbParams[db][dev][host];
+    $user = $dbParams[db][dev]['user'];
+    $password = $dbParams[db][dev]['password'];
+    $database = $dbParams[db][dev]['database'];
+    $usersTable = $dbParams[db][users]['table'];
+    */
+
+    $serverName = localhost;
+    $user = devdem;
+    $password = yurglazbr0uken666;
+    $database = rti1;
+    $usersTable = users_rti1;
+
